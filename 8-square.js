445dbc686facf126744717132caf4a0216b894cd
Henry-houdini <jimmy.johnson.incall@gmail.com>
2025-11-10 16:23:04 +0200

    repeating the square size

---
+++ 8-square.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (isNaN(size)) {
+    console.log("Missing size");
+} else {
+    for (i = 1; i <= size; i++) {
+        console.log("x".repeat(size));
+    }
+}
