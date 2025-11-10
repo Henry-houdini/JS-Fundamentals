2d695e8492ff71eadf9c77e1d429c014d890bf57
Henry-houdini <jimmy.johnson.incall@gmail.com>
2025-11-10 15:36:28 +0200

    loop practice

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+const number = parseInt(args[0]);
+
+if (isNaN(number)) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < number; i++) {
+        console.log(i)
+    }
+}
