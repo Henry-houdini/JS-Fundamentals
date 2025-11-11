2645545abe982150b72e77c4582ba5ee17323517
Henry-houdini <jimmy.johnson.incall@gmail.com>
2025-11-11 11:59:39 +0200

    correct the J

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 const args = process.argv.slice(2);
 
-const code_name = ["C is fun", "Python is cool", "javaScript is amazing"]
+const code_name = ["C is fun", "Python is cool", "JavaScript is amazing"]
 
 for (let i = 0; i < code_name.length; i++) {
     console.log(code_name[i]);
