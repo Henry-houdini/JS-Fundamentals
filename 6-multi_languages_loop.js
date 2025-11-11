d8ad13e1c1132120d50b578fce27ca6ce2379c99
Henry-houdini <jimmy.johnson.incall@gmail.com>
2025-11-11 11:55:50 +0200

    loops pratice

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+const args = process.argv.slice(2);
+
+const code_name = ["C is fun", "Python is cool", "javaScript is amazing"]
+
+for (let i = 0; i < code_name.length; i++) {
+    console.log(code_name[i]);
+}
